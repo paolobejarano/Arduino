c0705799ae5893c428311c73ee6e3b9240802ce1
Paolo <31755218+paolobejarano@users.noreply.github.com>
2018-09-13 18:23:00 -0500

    Update and rename BoxBotnny_version4.ino to carro.ino

---
+++ carro.ino
@@ -0,0 +1,44 @@
+//Motores de las llantas izquierda y derecha, y sus respectivos pines:
+int LM1 = 2;
+int LM2 = 3;
+int RM1 = 4;
+int RM2 = 5;
+
+void avanzar(){
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
+}
+
+void izquierda(){
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, HIGH);
+  digitalWrite(RM2, LOW);
+}
+
+void detener(){
+  digitalWrite(LM1, LOW);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
+}
+
+void derecha(){
+  digitalWrite(LM1, HIGH);
+  digitalWrite(LM2, LOW);
+  digitalWrite(RM1, LOW);
+  digitalWrite(RM2, LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LM1, OUTPUT);
+  pinMode(LM2, OUTPUT);
+  pinMode(RM1, OUTPUT);
+  pinMode(RM2, OUTPUT);
+}
+
+void loop() {
+}
